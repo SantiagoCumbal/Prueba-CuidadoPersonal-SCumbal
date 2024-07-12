ad57913223fd717949c2812d244a76a4742f78f8
1-21-01-E043-09\POO <santiago.cumbalepn.edu.ec>
2024-07-12 11:13:29 -0500

    Login1

---
+++ src/main/java/org/example/Login.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login {
+    public JPanel MainPanel;
+    private JTextField usuarioT;
+    private JTextField contraseñaT;
+    private JButton aceptarButton;
+    private JLabel Error;
+
+    public Login() {
+        aceptarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
